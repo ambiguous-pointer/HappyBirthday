9c5715f5c33e8647e5d0dac8108d1d4bd3b93f68
Comrade Yi <ambiguous-pointer@outlook.com>
2024-01-09 14:49:55 +0800

    暂存更改

--- static/images/jiao-jiao/main.py
+++ static/images/jiao-jiao/main.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import cv2
 import uuid
+from PIL import Image  
 
 def getFileNamesByPath(path):
     fileList = os.listdir(path)
@@ -11,6 +12,15 @@ def getFileNamesByPath(path):
             # getFileNamesByPath(pathTmp)
             pass
         elif os.path.isfile(pathTmp):
+            if filename.endswith('.webp'):  
+                # 构建源文件和目标文件的路径  
+                src_path = os.path.join(path, filename)  
+                dst_path = os.path.join(path, filename.replace('.webp', '.png'))  
+                # 打开webp图像  
+                img = Image.open(src_path)  
+                # 保存为png格式  
+                img.save(dst_path, 'PNG') 
+                os.remove(src_path)  
             if os.path.splitext(pathTmp)[-1] != ".py":
                 # print(pathTmp)
                 img  = cv2.imread(pathTmp)
@@ -19,15 +29,14 @@ def getFileNamesByPath(path):
                     continue
                 else:
                     targetObject = img.shape
-                s = "".join(["image-",str(uuid.uuid4()),"-",str(targetObject[0]),"-",str(targetObject[1]),os.path.splitext(pathTmp)[-1]])
+                s = "".join(["image-",str(uuid.uuid4()),"-",str(targetObject[1]),str(targetObject[0]),"-",os.path.splitext(pathTmp)[-1]])
                 print(os.path.join(path,s))
                 os.rename(pathTmp,os.path.join(path,s))
             else:
                 pass
 
     pass 
-
-
+ 
 if __name__ == '__main__':
     getFileNamesByPath(str(os.path.abspath("./")))
     pass
